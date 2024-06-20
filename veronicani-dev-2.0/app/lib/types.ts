5a53b198592ead7f273357844f224bb49a59222a
Veronica Ni <vni1294@gmail.com>
2024-06-19 21:06:57 -0400

    applied prettier

--- veronicani-dev-2.0/app/lib/types.ts
+++ veronicani-dev-2.0/app/lib/types.ts
@@ -2,36 +2,36 @@
 
 type tNavItem = {
   name: string;
-  anchor: string;
-}
+  href: string;
+};
 
 type tSocialLink = {
   name: string;
   url: string;
   icon: string;
-}
+};
 
 type tProject = {
   title: string;
   urls: {
     demo?: string;
     github?: string;
-  }
+  };
   loginCreds?: {
     guest?: {
       username: string;
       password: string;
-    }
+    };
     admin?: {
       username: string;
       password: string;
-    }
-  }
+    };
+  };
   tags: string[];
   imageUrl: string;
   description: string;
   notes?: string;
-}
+};
 
 type tExperience = {
   position: string;
@@ -39,18 +39,11 @@ type tExperience = {
   location: string;
   years: [number, number];
   description: string[];
-}
+};
 
 type tEmail = {
   displayText: string;
   url: string;
-}
+};
 
-
-export type {
-  tNavItem,
-  tSocialLink,
-  tProject,
-  tExperience,
-  tEmail,
-}
+export type { tNavItem, tSocialLink, tProject, tExperience, tEmail };
