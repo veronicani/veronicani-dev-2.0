07b82a6cbeb182d5ccac36fdcd0b75294a31d823
Veronica Ni <vni1294@gmail.com>
2024-06-19 22:16:04 -0400

    stubbed out about section

---
+++ veronicani-dev-2.0/app/components/About.tsx
@@ -0,0 +1,34 @@
+import SectionHeading from "./ui/SectionHeading";
+import Section from "./ui/Section";
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <section className="About">
+      {/* Insert Image component here */}
+      <Section heading="Hi!">
+        <p>
+          I'm a multi-disciplinary front-end software engineer and designer,
+          with a 5+ year background in illustration, creative advertising,
+          motion graphics, and mobile games. Currently working with the Live
+          Music Project.
+        </p>
+        <p>
+          I'm here to bridge the gap between engineering and design needs during
+          a product's development cycle, and contribute to products with a
+          design-first methodology to deliver impeccable user experiences.
+        </p>
+      </Section>
+      <Section heading="Previously...">
+        <p>
+          I've spearheaded design and branding for interactive digital
+          platforms, web + television animation, live events, photo shoots,
+          installations, digital + print media campaigns, and more as Art
+          Director at Black Math, Inc.
+        </p>
+        <p>I’ve been privileged to work with clients such as:</p>
+        {/* Insert BRAND icons here */}
+      </Section>
+    </section>
+  );
+}
