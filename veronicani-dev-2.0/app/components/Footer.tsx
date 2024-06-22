7576ce12bec90f2899c3c60d04f3584197bd1e09
Veronica Ni <vni1294@gmail.com>
2024-06-22 18:41:53 -0400

    added footer

---
+++ veronicani-dev-2.0/app/components/Footer.tsx
@@ -0,0 +1,20 @@
+/** Footer.
+ *
+ * Props:
+ * - none
+ *
+ * State:
+ * - none
+ *
+ * App -> Footer
+ */
+export default function Footer() {
+  return (
+    <footer>
+      <small>
+        Drafted in Figma, built with Next.js and Tailwind. Deployed with Vercel.
+        Bootstrapped with <i className="bi bi-stars"></i> by yours truly 2024.
+      </small>
+    </footer>
+  );
+}
