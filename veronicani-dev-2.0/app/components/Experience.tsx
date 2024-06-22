e0fb5baaff2eaf968aea249bfed5adb82b8de1b1
Veronica Ni <vni1294@gmail.com>
2024-06-22 18:41:38 -0400

    added experience

---
+++ veronicani-dev-2.0/app/components/Experience.tsx
@@ -0,0 +1,32 @@
+import { EXPERIENCES } from "../lib/data";
+import Section from "./ui/Section";
+import ExperienceCard from "./ui/ExperienceCard";
+import ButtonLink from "./ui/ButtonLink";
+
+/** Experience section.
+ *
+ * Props:
+ * - none
+ *
+ * State:
+ * - none
+ *
+ * App -> Experience -> ExperienceCard[]
+ */
+export default function Experience() {
+  return (
+    <Section heading="Experience">
+      {EXPERIENCES.map((experience) => (
+        <ExperienceCard
+          key={`${experience.position}${experience.company}`}
+          experience={experience}
+        />
+      ))}
+      <ButtonLink
+        icon="bi-link-45deg"
+        text="Resume PDF"
+        url="/static/files/resume.pdf"
+      />
+    </Section>
+  );
+}
---
+++ veronicani-dev-2.0/app/components/ui/ExperienceCard.tsx
@@ -0,0 +1,43 @@
+import Card from "./Card";
+import Accomplishment from "./Accomplishment";
+import { tExperience } from "@/app/lib/types";
+
+type tExperienceCardProps = { experience: tExperience };
+
+/** Experience card component.
+ *
+ * Props:
+ * - experience: experience data object
+ * State:
+ * - none
+ *
+ * Experience -> Section -> { SectionHeading, ExperienceCard[] }
+ */
+
+export default function ExperienceCard({ experience }: tExperienceCardProps) {
+  const { position, company, location, years, description } = experience;
+
+  return (
+    <Card className="Experience-card">
+      <div>
+        <h3>{position}</h3>
+        <h4>{company}</h4>
+        <div>
+          <div>
+            <h5>{years[0]}</h5>
+            {years[1] && <h5>{` - ${years[1]}`}</h5>}
+          </div>
+          <h5>{location}</h5>
+        </div>
+      </div>
+      <ol>
+        {description.map((d) => (
+          <Accomplishment
+            key={d}
+            description={d}
+          />
+        ))}
+      </ol>
+    </Card>
+  );
+}
