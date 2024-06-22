f3c09373a2514a8669c15795813610141cfc158e
Veronica Ni <vni1294@gmail.com>
2024-06-22 19:47:53 -0400

    added heading

---
+++ veronicani-dev-2.0/app/components/Heading.tsx
@@ -0,0 +1,18 @@
+/** Heading.
+ *
+ * Props:
+ * - none
+ *
+ * State:
+ * - none
+ *
+ * App -> Heading
+ */
+export default function Heading() {
+  return (
+    <div>
+      <h1>Veronica Ni</h1>
+      <h2>Design-first Software Engineer</h2>
+    </div>
+  );
+}
