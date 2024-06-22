b93f88c8b684b035cd3a75715c1dffdf86d6c4a9
Veronica Ni <vni1294@gmail.com>
2024-06-22 19:48:54 -0400

    added components to main page

--- veronicani-dev-2.0/app/page.tsx
+++ veronicani-dev-2.0/app/page.tsx
@@ -4,20 +4,25 @@ import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Heading from "./components/Heading";
+import SocialMedia from "./components/SocialMedia";
 
 export default function Home() {
   return (
-    <>
-      <header>
+    <div className="lg:flex lg:justify-between lg:gap-4">
+      <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/4
+        lg:flex-col lg:justify-between">
+        <Heading />
         <Navigation />
+        <SocialMedia />
       </header>
-      <main>
+      <main className="lg:w-3/4">
         <About />
         <Projects />
         <Experience />
         <Contact />
         <Footer />
       </main>
-    </>
+    </div>
   );
 }
