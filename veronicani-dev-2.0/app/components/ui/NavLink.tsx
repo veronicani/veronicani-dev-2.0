30c77902351f7f8c43b9e6a4fa48b0757b751409
Veronica Ni <vni1294@gmail.com>
2024-06-30 16:24:18 -0400

    styled nav indicator to be cross + added animation

--- veronicani-dev-2.0/app/components/ui/NavLink.tsx
+++ veronicani-dev-2.0/app/components/ui/NavLink.tsx
@@ -2,14 +2,15 @@ import { tNavItem } from "@/app/lib/types";
 import Link from "next/link";
 import useHash from "../hooks/useHash";
 import applyPrefix from "../helpers/applyPrefix";
+import styles from './NavLink.module.css';
 
 export default function NavLink({ link }: { link: tNavItem }) {
   const isActive = link.href === useHash();
 
-  const navIndicatorInactive = "w-[2px] bg-gray-400";
-  const navIndicatorActive = "w-8 bg-blue-500";
+  const navIndicatorInactive = "w-2 bg-gray-400";
+  const navIndicatorActive = "w-12 bg-blue-500";
   const navIndicatorHover = applyPrefix("group-hover", navIndicatorActive);
-  
+
   const navTextInactive = "text-gray-dark ";
   const navTextActive = "font-bold text-blue-500";
   const navTextHover = applyPrefix("group-hover", navTextActive);
@@ -17,18 +18,24 @@ export default function NavLink({ link }: { link: tNavItem }) {
   return (
     <Link
       href={`${link.href}`}
-      className="flex items-center gap-2 group "
+      className={`group flex items-center gap-2 ${styles['spin-hover']}`}
     >
-      <div
-        className={`h-[2px] rounded transition-all motion-reduce:transition-none
-        ${navIndicatorHover}
-         ${isActive ? navIndicatorActive : navIndicatorInactive}
-        `}
-      ></div>
+      <div className={`nav-cross flex items-center ${!isActive ? styles['spin-element'] : ""}`}>
+        <span
+          className={`z-1 absolute start-1 h-2 w-[2px] -translate-x-1/2 rounded transition-all group-hover:bg-blue-500 motion-reduce:transition-none ${
+            isActive ? "bg-blue-500" : "bg-gray-400"
+          }`}
+        ></span>
+        <span
+          className={`h-[2px] rounded transition-all motion-reduce:transition-none ${navIndicatorHover} ${
+            isActive ? navIndicatorActive : navIndicatorInactive
+          }`}
+        ></span>
+      </div>
       <span
-        className={`transition-all motion-reduce:transition-none
-        ${navTextHover}
-        ${isActive ? navTextActive : navTextInactive}`}
+        className={`transition-all motion-reduce:transition-none ${navTextHover} ${
+          isActive ? navTextActive : navTextInactive
+        }`}
       >
         {link.name}
       </span>
