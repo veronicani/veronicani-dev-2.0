f2bedce1c133f49a45cb3b70fa41e7e35c5742fa
Veronica Ni <vni1294@gmail.com>
2024-06-30 12:03:38 -0400

    added card styling on hover

--- veronicani-dev-2.0/app/components/ui/Card.tsx
+++ veronicani-dev-2.0/app/components/ui/Card.tsx
@@ -7,7 +7,9 @@ export default function Card({ className, children }: tCardProps) {
       className={`${className} group flex flex-col gap-1 p-4
       border-b-4 md:border-b-2 lg:border-b-0 border-gray-200 -mx-4 lg:-mx-0
       lg:group-hover/list:opacity-50 lg:hover:!opacity-100 
-      lg:p-6 lg:-ml-6 lg:hover:shadow-xl lg:rounded-lg
+      lg:p-6 lg:-ml-6 lg:rounded-lg
+      lg:hover:-translate-y-3 lg:hover:-translate-x-3 
+      lg:hover:shadow-[3px_4px_0px_3px_#2462FF] lg:hover:border-blue-500 lg:hover:border-2
       transition motion-reduce:transition-none`}
     >
       {children}
@@ -16,3 +18,6 @@ export default function Card({ className, children }: tCardProps) {
     </>
   );
 }
+
+
+// lg:hover:shadow-[0_0_45px_9px_rgba(0,0,0,0.1)]
