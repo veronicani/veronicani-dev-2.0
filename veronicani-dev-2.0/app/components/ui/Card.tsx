d6c006a4a450d7e619c954ca1206cdb91a064462
Veronica Ni <vni1294@gmail.com>
2024-07-08 13:05:44 -0400

    removed border from last card

--- veronicani-dev-2.0/app/components/ui/Card.tsx
+++ veronicani-dev-2.0/app/components/ui/Card.tsx
@@ -5,7 +5,7 @@ export default function Card({ className, children }: tCardProps) {
     <>
     <li
       className={`${className} group flex flex-col gap-1 p-4
-      border-b-4 md:border-b-2 lg:border-b-0 border-gray-200 -mx-4 lg:-mx-0
+      border-b-4 md:border-b-2 lg:border-b-0 border-gray-200 last-of-type:border-b-0 -mx-4 lg:-mx-0
       lg:group-hover/list:opacity-50 lg:hover:!opacity-100 
       lg:p-6 lg:-ml-6 lg:rounded-lg
       lg:hover:-translate-y-3 lg:hover:-translate-x-3 
