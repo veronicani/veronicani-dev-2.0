edcd08bc01d93ad9044bd6f7b317077ca3b86641
Veronica Ni <vni1294@gmail.com>
2024-07-08 13:18:50 -0400

    fixed escaping ' chars

--- veronicani-dev-2.0/app/components/About.tsx
+++ veronicani-dev-2.0/app/components/About.tsx
@@ -18,25 +18,25 @@ export default function About() {
       {/* Insert Image component here */}
       <Section heading="Hi!" id="about-hi">
         <p>
-          I'm a multi-disciplinary front-end software engineer and designer,
+          {`I'm a multi-disciplinary front-end software engineer and designer,
           with a 5+ year background in illustration, creative advertising,
           motion graphics, and mobile games. Currently working with the Live
-          Music Project.
+          Music Project.`}
         </p>
         <p>
-          I'm here to bridge the gap between engineering and design needs during
+          {`I'm here to bridge the gap between engineering and design needs during
           a product's development cycle, and contribute to products with a
-          design-first methodology to deliver impeccable user experiences.
+          design-first methodology to deliver impeccable user experiences.`}
         </p>
       </Section>
       <Section heading="Previously..." id="about-previously">
         <p>
-          I've spearheaded design and branding for interactive digital
+          {`I've spearheaded design and branding for interactive digital
           platforms, web + television animation, live events, photo shoots,
           installations, digital + print media campaigns, and more as Art
-          Director at Black Math, Inc.
+          Director at Black Math, Inc.`}
         </p>
-        <p>I’ve been privileged to work with clients such as:</p>
+        <p>{`I’ve been privileged to work with clients such as:`}</p>
         <BrandLogos />
       </Section>
     </section>
