c43e39dcbc20d1d0e2c87efaaa09158f66eab599
Veronica Ni <vni1294@gmail.com>
2024-07-07 21:25:38 -0400

    added sans font

--- veronicani-dev-2.0/app/styles/fonts.ts
+++ veronicani-dev-2.0/app/styles/fonts.ts
@@ -1,4 +1,4 @@
-import { Fira_Code, DM_Serif_Display } from 'next/font/google';
+import { Fira_Code, DM_Serif_Display, DM_Sans } from 'next/font/google';
 
 export const fira = Fira_Code({ 
   subsets: ['latin'],
@@ -9,4 +9,8 @@ export const dm_serif = DM_Serif_Display({
   weight: '400',
   subsets: ['latin'],
   variable: '--font-dm-serif',
+})
+export const dm_sans = DM_Sans({
+  subsets: ['latin'],
+  variable: '--font-dm-sans',
 })
