d3e84f1f98bf09ed39433a1a3a9b3369d1af9913
Veronica Ni <vni1294@gmail.com>
2025-12-14 19:50:54 -0500

    Update about section

--- veronicani-dev-2.0/app/components/About.tsx
+++ veronicani-dev-2.0/app/components/About.tsx
@@ -18,25 +18,40 @@ export default function About() {
       {/* Insert Image component here */}
       <Section heading="Hi!" id="about-hi">
         <p>
-          I&apos;m a multi-disciplinary front-end software engineer and designer,
-          with a 5+ year background in illustration, creative advertising,
-          motion graphics, and mobile games. Currently working on UI development
-          at Naviguide Inc.
+          User-centric software engineer with strong project ownership and
+          and collaboration skills from previous experience in UX design, 
+          creative advertising, and education. I combine my UI/UX expertise to
+          deliver featuers that exceed expectations, focusing on well-designed,
+          robustly tested, and scalable code.
         </p>
-        <p>
-          I&apos;m here to bridge the gap between engineering and design needs during
-          a product&apos;s development cycle, and contribute to products with a
-          design-first methodology to deliver impeccable user experiences.
-        </p>
+        <p>🤝 <strong>I start every task by asking questions and listening</strong>,
+         to ensure I understand the problem completely before implementing a solution.</p>
+        <p>🗺️ <strong>Adept at taking full ownership of projects</strong> and
+         breaking down complex, difficult goals into manageable tasks with
+         clear documentation and defined next steps.</p>
+        <p>🎨 <strong>Leveraging hands-on enterprise UI/UX experience</strong>,
+         I ensure designers understand technical limits without jargon, making
+         sure no design details fall through the cracks during development.</p>
+        <p>🥳 <strong>I take the work seriously but not myself.</strong> I
+         celebrate others on my team and handle difficult situations with humor,
+         actively boosting morale by finding the fun in small things.</p>
+        <p>📚 <strong>I foster a continuous learning culture</strong> to elevate
+         team performance and adhere to best practices by actively asking
+         questions, promoting knowledge sharing.</p>
       </Section>
       <Section heading="Previously..." id="about-previously">
+        <p>
+          I designed and taught K-12 lesson plans across six Japanese public
+          schools, successfully operating in a 100% Japanese workplace after
+          self-teaching N3 Japanese and business etiquette.
+        </p>
         <p>
           I&apos;ve spearheaded design and branding for interactive digital
           platforms, web + television animation, live events, photo shoots,
           installations, digital + print media campaigns, and more as Art
-          Director at Black Math, Inc.
+          Director at Black Math Inc. I’ve been privileged to work with clients 
+          such as:
         </p>
-        <p>I’ve been privileged to work with clients such as:</p>
         <BrandLogos />
       </Section>
     </section>
